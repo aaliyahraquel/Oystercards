f396eebc8494316c801f6324ac92e6f35d0dd6c0
Aaliyah Johnson <aaliyahjohnson@BGL-MAC-008905.bgl.financial.local>
2019-09-24 17:10:23 +0100

    touch in, touch out and in journey?

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,13 +1,36 @@
 class Oystercard
 
 attr_reader :balance
+attr_accessor :in_journey
+
+LIMIT = 90
+MIN_BALANCE = 1
 
   def initialize
     @balance = 0
+    @in_journey = false
+
   end
 
   def top_up(credit)
-    # fail "Limit exceeded.(Max £90)" if @balance + credit > 90
+    fail "Limit exceeded.(Max £#{LIMIT})" if @balance + credit > LIMIT
     @balance += credit
+  end
+
+  def deduct(debit)
+    @balance -= debit
+  end
+
+  def touch_in
+    fail "Please top up!" if @balance < MIN_BALANCE
+    @in_journey = true
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,7 +13,35 @@ let (:new_card) { Oystercard.new }
   end
 
   it 'raises error if top up method would make the balance over 90' do
-    expect{ new_card.top_up(91) }.to raise_error "Limit exceeded.(Max £90)"
+    max_balance = Oystercard::LIMIT
+    new_card.top_up(max_balance)
+    expect { new_card.top_up(1)}.to raise_error "Limit exceeded.(Max £90)"
   end
 
-end
+  it 'deducts the fare from the card' do
+    new_card.top_up(10)
+    expect(new_card.deduct(5)).to eq(5)
+  end
+
+  it 'can be touched-in' do
+    new_card.top_up(5)
+    new_card.touch_in
+    expect(new_card).to be_in_journey
+  end
+
+  it 'can return whether it is in journey' do
+    expect(new_card).to respond_to(:in_journey)
+  end
+
+  it 'can be touched_out' do
+    new_card.touch_out
+    expect(new_card).not_to be_in_journey
+  end
+
+  it 'has a minimumm balance of £1' do
+    expect {new_card.touch_in}.to raise_error "Please top up!"
+  end
+
+
+
+  end
